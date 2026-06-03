36a03702c44ba80fe260c185ce87bc5a1d418a99
Marchen <957549315@qq.com>
2026-06-03 12:51:16 +0800

    修复uigf导入uid为int导致导入失败的问题

--- FufuLauncher/Models/UIGF/UIGFEntry.cs
+++ FufuLauncher/Models/UIGF/UIGFEntry.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace FufuLauncher.Models.UIGF;
@@ -5,6 +6,7 @@ namespace FufuLauncher.Models.UIGF;
 public class UIGFEntry
 {
     [JsonPropertyName("uid")]
+    [JsonConverter(typeof(JsonStringOrNumberConverter))]
     public string Uid { get; set; }
 
     [JsonPropertyName("timezone")]
@@ -15,4 +17,19 @@ public class UIGFEntry
 
     [JsonPropertyName("list")]
     public List<UIGFItem> List { get; set; } = new();
+}
+
+public class JsonStringOrNumberConverter : JsonConverter<string>
+{
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Number)
+            return reader.GetInt64().ToString();
+        if (reader.TokenType == JsonTokenType.String)
+            return reader.GetString() ?? "";
+        return "";
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value);
 }
